e1d1ec1a35711c341988cf1dda6812219174698e
Junbin Bao <151312386+Junbin3600@users.noreply.github.com>
2023-11-20 08:07:03 +0530

    Update RetailerController.java

--- Virtual_Stock_Api/src/main/java/com/topex/Virtual_Stock_Api/controller/RetailerController.java
+++ Virtual_Stock_Api/src/main/java/com/topex/Virtual_Stock_Api/controller/RetailerController.java
@@ -1,16 +1,20 @@
 package com.topex.Virtual_Stock_Api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.topex.Virtual_Stock_Api.model.Product;
 import com.topex.Virtual_Stock_Api.model.Supplier;
 import com.topex.Virtual_Stock_Api.service.SupplierService;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v4/order")
